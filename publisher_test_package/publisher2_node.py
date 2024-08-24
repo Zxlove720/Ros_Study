806bfd6aaf97ca3b45ba76b412f3e1c6fb49f13d
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-08-24 22:39:20 +0800

    Add files via upload
    ROS publisher_node exercise 2024.8.24

---
+++ publisher_test_package/publisher2_node.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+import rospy
+from std_msgs.msg import String
+
+if __name__ == '__main__':
+    # 初始化节点
+    rospy.init_node("python_publisher2_node")
+    # 发送警告日志，表明节点开始正常运行
+    rospy.logwarn("python_publisher2_node is run")
+    # 创建话题发布者对象
+    publisher = rospy.Publisher("python2_topic", String, queue_size=10)
+    # 创建控制话题发布的频率的对象
+    rate = rospy.Rate(2)
+
+    while not rospy.is_shutdown():
+        # 循环条件：节点没有被关闭
+        # 发送消息日志，表明节点还在运行
+        rospy.loginfo("python2_publisher_node still running")
+        # 创建消息
+        message = String()
+        message.data = "python2_publisher_node send a message"
+        # 发送消息
+        publisher.publish(message)
+        # 控制频率
+        rate.sleep()
